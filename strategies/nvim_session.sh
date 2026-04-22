ac925e42e171bf0f5a6109f31bed05567408034a
Urtzi Enriquez-Urzelai <urtzi.enriquez@gmail.com>
2026-04-22 16:22:06 +0200

    also restore local shada if present in the directory

--- strategies/nvim_session.sh
+++ strategies/nvim_session.sh
@@ -2,7 +2,8 @@
 
 # "nvim session strategy"
 #
-# Same as vim strategy, see file 'vim_session.sh'
+# Restore nvim with Session.vim and/or Session.shada when present.
+# Falls back to plain nvim if neither file exists.
 
 ORIGINAL_COMMAND="$1"
 DIRECTORY="$2"
@@ -11,20 +12,20 @@ nvim_session_file_exists() {
 	[ -e "${DIRECTORY}/Session.vim" ]
 }
 
-original_command_contains_session_flag() {
-	[[ "$ORIGINAL_COMMAND" =~ "-S" ]]
+nvim_shada_file_exists() {
+	[ -e "${DIRECTORY}/Session.shada" ]
 }
 
 main() {
-	if nvim_session_file_exists; then
+	if nvim_session_file_exists && nvim_shada_file_exists; then
+		echo "nvim -i '${DIRECTORY}/Session.shada' -S '${DIRECTORY}/Session.vim'"
+	elif nvim_session_file_exists; then
 		echo "nvim -S"
-	elif original_command_contains_session_flag; then
-		# Session file does not exist, yet the original nvim command contains
-		# session flag `-S`. This will cause an error, so we're falling back to
-		# starting plain nvim.
-		echo "nvim"
+	elif nvim_shada_file_exists; then
+		echo "nvim -i '${DIRECTORY}/Session.shada'"
 	else
 		echo "$ORIGINAL_COMMAND"
 	fi
 }
+
 main
